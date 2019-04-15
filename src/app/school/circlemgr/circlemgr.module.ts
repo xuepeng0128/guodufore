ed9890f8d9e4e09276bdef167cb900b6ca9d2f18
yxpmatrix <2426574593@qq.com>
2019-04-15 22:44:08 +0800

    new project

--- src/app/school/circlemgr/circlemgr.module.ts
+++ src/app/school/circlemgr/circlemgr.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CirclemgrRoutingModule } from './circlemgr-routing.module';
 import { SchoolCircleComponent } from './school-circle/school-circle.component';
-<<<<<<< HEAD
+
 
 @NgModule({
   declarations: [ SchoolCircleComponent],
-=======
-import { SchoolManagerHabitComponent } from './school-manager-habit/school-manager-habit.component';
-import { TeacherHabitComponent } from './teacher-habit/teacher-habit.component';
 
-@NgModule({
-  declarations: [ SchoolCircleComponent, SchoolManagerHabitComponent, TeacherHabitComponent],
->>>>>>> 3b8492a2de1d4e167415f732a07d04935f29e0e2
   imports: [
     CommonModule,
     CirclemgrRoutingModule
