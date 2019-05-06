00bc10f243bff0dc296ae95dd34b537e56243cbc
yxpmatrix <2426574593@qq.com>
2019-05-06 18:01:51 +0800

    new project

--- src/app/entity/ClassesTeacher.ts
+++ src/app/entity/ClassesTeacher.ts
@@ -5,18 +5,20 @@ export class ClassesTeacher {
   teacherName: string;
   studySubjectId: string;
   studySubjectName: string;
+  schoolStyle: number;
   regTime: Date;
   endTime: Date;
 
 
   constructor(options: {id?: number, classesId?: string, teacherId?: string, teacherName?: string,
-                       studySubjectId?: string, studySubjectName?: string, regTime?: Date, endTime?: Date } = {}) {
+                       studySubjectId?: string, studySubjectName?: string, schoolStyle?: number, regTime?: Date, endTime?: Date } = {}) {
     this.id = options.id ;
     this.classesId = options.classesId || '';
     this.teacherId = options.teacherId || '';
     this.teacherName = options.teacherName || '';
     this.studySubjectId = options.studySubjectId || '';
     this.studySubjectName = options.studySubjectName || '';
+    this.schoolStyle = options.schoolStyle || 1;
     this.regTime = options.regTime || new Date();
     this.endTime = options.endTime;
   }
--- src/app/entity/Student.ts
+++ src/app/entity/Student.ts
@@ -1,16 +1,32 @@
 export class Student {
+  id: number;
   studentPaperId: string;
+  studentId: string;
   studentName: string;
+  sex: number;
   birthday: Date;
-  sex: string;
-  address: string;
+  schoolId: string;
+  address: string ; // 家庭住址
   tel: string;
-  constructor(options: {studentPaperId?: string, studentName?: string, birthday?: Date, sex?: string, address?: string, tel?: string}= {}) {
+  headimg: string;
+  nickname: string;
+  regTime: Date;
+  wxcode: string; // 小程序openid
+  constructor(options: {id?: number, studentPaperId?: string, studentId?: string, studentName?: string,
+                        sex?: number, birthday?: Date, schoolId?: string, address?: string, tel?: string,
+                         headimg?: string, nickname?: string, regTime?: Date, wxcode?: string}= {}) {
+    this.id = options.id;
     this.studentPaperId = options.studentPaperId || '';
+    this.studentId = options.studentId || '';
     this.studentName = options.studentName || '';
-    this.birthday = options.birthday || new Date();
-    this.sex = options.sex || '1';
+    this.sex = options.sex || 1;
+    this.birthday = options.birthday;
+    this.schoolId = options.schoolId || '';
     this.address = options.address || '';
     this.tel = options.tel || '';
+    this.headimg = options.headimg || '';
+    this.nickname = options.nickname || '';
+    this.regTime = options.regTime;
+    this.wxcode = options.wxcode || '';
   }
 }
