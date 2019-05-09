3fcc7287614adcbcbe79eaf927f45b7df7e139c3
yxpmatrix <2426574593@qq.com>
2019-05-09 17:17:58 +0800

    new project

--- src/app/entity/Habit.ts
+++ src/app/entity/Habit.ts
@@ -1,54 +1,54 @@
 export class Habit {
-  habitId:string;
+  habitId: string;
   circleId: string;
-  habitClassId : string;
-  subHabitClassId : string;
-  icon : string;
-  color : string;
-  habitName : string;
-  memo : string;
-  picUrl : string;
-  pirTime : number;
-  timeUnit : string;
-  mode : number;
-  timeModeNum : string;
-  countModeNum : number;
-  valueModeNum :number;
-  unitName : string;
-  guoduCoin : number;
-  score   : number;
-  putCardExamId : string;
-  buildTime : Date;
-  buildTeacherId : string;
-  buildStudentId : string;
+  habitClassId: string;
+  subHabitClassId: string;
+  icon: string;
+  color: string;
+  habitName: string;
+  memo: string;
+  picUrl: string;
+  pirTime: number;
+  timeUnit: string;
+  mode: number;
+  timeModeNum: string;
+  countModeNum: number;
+  valueModeNum: number;
+  unitName: string;
+  guoduCoin: number;
+  score: number;
+  putCardExamId: string;
+  buildTime: Date;
+  buildTeacherId: string;
+  buildStudentId: string;
 
 
-  constructor(habitId: string, circleId: string, habitClassId: string, subHabitClassId: string,
-                icon: string, color: string, habitName: string, memo: string, picUrl: string,
-                pirTime: number, timeUnit: string, mode: number, timeModeNum: string, countModeNum: number,
-                valueModeNum: number, unitName: string, guoduCoin: number, score: number, putCardExamId: string,
-                buildTime: Date, buildTeacherId: string, buildStudentId: string) {
-    this.habitId = habitId;
-    this.circleId = circleId;
-    this.habitClassId = habitClassId;
-    this.subHabitClassId = subHabitClassId;
-    this.icon = icon;
-    this.color = color;
-    this.habitName = habitName;
-    this.memo = memo;
-    this.picUrl = picUrl;
-    this.pirTime = pirTime;
-    this.timeUnit = timeUnit;
-    this.mode = mode;
-    this.timeModeNum = timeModeNum;
-    this.countModeNum = countModeNum;
-    this.valueModeNum = valueModeNum;
-    this.unitName = unitName;
-    this.guoduCoin = guoduCoin;
-    this.score = score;
-    this.putCardExamId = putCardExamId;
-    this.buildTime = buildTime;
-    this.buildTeacherId = buildTeacherId;
-    this.buildStudentId = buildStudentId;
+  constructor(options: { habitId?: string, circleId?: string, habitClassId?: string, subHabitClassId?: string,
+              icon?: string, color?: string, habitName?: string, memo?: string, picUrl?: string,
+              pirTime?: number, timeUnit?: string, mode?: number, timeModeNum?: string, countModeNum?: number,
+              valueModeNum?: number, unitName?: string, guoduCoin?: number, score?: number, putCardExamId?: string,
+              buildTime?: Date, buildTeacherId?: string, buildStudentId?: string } = {}) {
+    this.habitId = options.habitId || '';
+    this.circleId = options.circleId || '';
+    this.habitClassId = options.habitClassId || '';
+    this.subHabitClassId = options.subHabitClassId || '';
+    this.icon = options.icon || '';
+    this.color = options.color || '';
+    this.habitName = options.habitName || '';
+    this.memo = options.memo || '';
+    this.picUrl = options.picUrl || '';
+    this.pirTime = options.pirTime || 1;
+    this.timeUnit = options.timeUnit || '';
+    this.mode = options.mode || 1;
+    this.timeModeNum = options.timeModeNum  || '';
+    this.countModeNum = options.countModeNum || 1;
+    this.valueModeNum = options.valueModeNum || 1;
+    this.unitName = options.unitName || '';
+    this.guoduCoin = options.guoduCoin || 0 ;
+    this.score = options.score || 0;
+    this.putCardExamId = options.putCardExamId || '';
+    this.buildTime = options.buildTime ;
+    this.buildTeacherId = options.buildTeacherId || '';
+    this.buildStudentId = options.buildStudentId || '';
   }
 }
